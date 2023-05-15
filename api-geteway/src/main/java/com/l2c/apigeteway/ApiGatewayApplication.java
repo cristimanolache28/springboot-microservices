8e87da1a49d6496ab97ab38c9f99dffba688b752
Cristian Manolache <110411359+cristian-manolache@users.noreply.github.com>
2023-05-15 16:33:53 +0300

    create API Gateway routes

---
+++ api-geteway/src/main/java/com/l2c/apigeteway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.l2c.apigeteway;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.net.http.HttpClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}

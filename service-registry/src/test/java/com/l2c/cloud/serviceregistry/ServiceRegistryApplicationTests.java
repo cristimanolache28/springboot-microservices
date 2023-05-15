ebaaa9323a596625fb63abb73c7e111d7bd41c60
Cristian Manolache <110411359+cristian-manolache@users.noreply.github.com>
2023-05-15 13:46:23 +0300

    add service-registry microservice

---
+++ service-registry/src/test/java/com/l2c/cloud/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2c.cloud.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

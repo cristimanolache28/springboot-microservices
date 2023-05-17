f0425446154eb8e63f2d2e94b7fb4f32547f9d59
Cristian Manolache <110411359+cristian-manolache@users.noreply.github.com>
2023-05-17 10:50:49 +0300

    add configuration file for microservices on repository (GitHub)

--- employee-service/src/main/java/com/l2c/employee/controller/EmployeeController.java
+++ employee-service/src/main/java/com/l2c/employee/controller/EmployeeController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/employees")
 @AllArgsConstructor
 public class EmployeeController {
-
     public EmployeeService employeeService;
 
     @PostMapping

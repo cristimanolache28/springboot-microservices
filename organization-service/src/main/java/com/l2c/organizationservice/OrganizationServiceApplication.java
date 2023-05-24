d55b2abdc1f29578e6f71d3527edf1a6d30e4e34
Cristian Manolache <110411359+cristian-manolache@users.noreply.github.com>
2023-05-24 16:13:20 +0300

    small changes

---
+++ organization-service/src/main/java/com/l2c/organizationservice/OrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.l2c.organizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationServiceApplication.class, args);
+	}
+
+}
